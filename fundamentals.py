02de07d441cd0a4dbd30eddc2266de6b1cc24626
leventarican <levent@protonmail.com>
2019-11-03 23:26:41 +0100

    pixels, read, write, display image

--- fundamentals.py
+++ fundamentals.py
@@ -1,8 +1,14 @@
 import cv2
 
+def display():
+    windows_id = 'imageviewer'
+    cv2.imshow(windows_id, img)
+    pressed_key = cv2.waitKey(0)
+    cv2.destroyWindow(windows_id)
+
 imagepath = 'number_zero.jpg'
 img = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
-print(img)
+# print(img)
 
 # [[  1   0   3   0   3   0   3   2   4   2   0]
 #  [  0   1   0   3   3 253 253   0   0   2   1]
@@ -17,3 +23,53 @@ print(img)
 #  [  0   0   5   0 254   0   0 255 254   0   1]
 #  [  0   8   0   3 253 253 255 250   1   2   1]
 #  [  2   0   0   0   5   0   4   1   3   0   0]]
+
+# ################################################
+# access a pixel
+
+y = 0   # row
+x = 0   # column
+print(img[y,x])
+
+# manipulate an image / modify a pixel
+img[y,x] = 255
+print(img)
+
+# region of interest
+# print(img[0:2,2])
+# print(img[1:2,5:7])
+# print(img[1,5:7])
+
+img[1,5:7] = 100
+print(img)
+
+# ################################################
+# display image with opencv
+
+windows_id = 'imageviewer'
+cv2.imshow(windows_id, img)
+
+# wait infinitely or give some ms
+# returns ascii code of the key
+pressed_key = cv2.waitKey(0)
+
+# cv2.destroyAllWindows()
+cv2.destroyWindow(windows_id)
+
+# or with matlab
+# import matplotlib.pyplot as plt
+# plt.imshow(img)
+# plt.colorbar()
+
+# ################################################
+# write an image
+
+# imwrite: filename, image, params (extention, quality, ...)
+cv2.imwrite('zero_cv2.jpg', img)
+display()
+
+# ################################################
+# read a color image
+
+channels = 3
+channels_transparency = 4
