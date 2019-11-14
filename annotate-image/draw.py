598f66ef25ee58590865ab042da227c618ccafb6
leventarican <levent@protonmail.com>
2019-11-14 23:43:03 +0100

    rectangle, ellipse, text

--- annotate-image/draw.py
+++ annotate-image/draw.py
@@ -24,10 +24,35 @@ def draw_circle(img):
     # fill circle
     cv2.circle(img, (50, 200), radius=30, color=(0, 255, 0), thickness=-1)
 
+def draw_ellipse(img):
+    center = (200, 200)
+    axes = (40, 20) # radius of the ellipse major and minor axes
+    angle = 0   # rotate ellipse
+    start_angle = 0
+    end_angle = 360
+    cv2.ellipse(img, center, axes, angle, start_angle, end_angle, color=(0, 0, 255), thickness=4)
+    # rotate the next ellipse (=90°), end angle = 180, half fill (just a negativ thickness)
+    cv2.ellipse(img, (200, 250), axes, 90, start_angle, 180, color=(0, 255, 255), thickness=-2)
+
+def draw_rectangle(img):
+    top_left_vertex = (250, 250)
+    bottom_right_vertex = (300, 300)
+    cv2.rectangle(img, top_left_vertex, bottom_right_vertex, color=(200, 0, 0), thickness=-1)
+
+def draw_text(img):
+    text = 'hello lion'
+    bottom_left_corner = (100, 400)
+    font_face = cv2.FONT_HERSHEY_COMPLEX
+    font_scale = 1.5
+    cv2.putText(img, text, bottom_left_corner, font_face, font_scale, color=(0,255,0), thickness=2)
+
 if __name__ == "__main__":
     path = '../lion.jpg'
     img = load_image(path)
     draw_line(img)
     draw_circle(img)
+    draw_ellipse(img)
+    draw_rectangle(img)
+    draw_text(img)
 
     display_image(img)
